f5029916624477a229a209afcda8de8babbe4e19
mp4linkedin <136432267+mp4linkedin@users.noreply.github.com>
2025-06-16 18:41:17 +0200

    Update pmt.py

--- pmt.py
+++ pmt.py
@@ -205,9 +205,9 @@ for url in urls:
 
                     if diff > 0.04:
                         add_diff(f"⏫ +{diff * 100:.2f}% - {title}")
-                    elif 0.01 < diff <= 0.04:
+                    elif 0.001 < diff <= 0.04:
                         add_diff(f"🔼 +{diff * 100:.2f}% - {title}")
-                    elif -0.04 <= diff < -0.01:
+                    elif -0.04 <= diff < -0.001:
                         add_diff(f"🔽 {diff * 100:.2f}% - {title}")
                     elif diff < -0.04:
                         add_diff(f"⏬ {diff * 100:.2f}% - {title}")
