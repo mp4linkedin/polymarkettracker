9273f0904d92548ec90907f9ad999cac1bec6e91
mp4linkedin <136432267+mp4linkedin@users.noreply.github.com>
2025-06-16 13:40:56 +0200

    Update pmt.py

--- pmt.py
+++ pmt.py
@@ -1,10 +1,11 @@
 import streamlit as st
 import psycopg2
+from datetime import datetime
 
 conn = psycopg2.connect(
     host="aws-0-eu-north-1.pooler.supabase.com",
     database="postgres",
-    user="postgres.yoapzdfznvualrngslfs",  # note the format: user.projectref
+    user="postgres.yoapzdfznvualrngslfs",
     password="DDeras22",
     port=6543
 )
@@ -12,8 +13,9 @@ cursor = conn.cursor()
 
 params = st.query_params
 if params.get("add", "").lower() == "true":
+    unique_title = "entry_" + datetime.utcnow().isoformat()
     cursor.execute("""
-    INSERT INTO counter (market_title, created_at, market_value, p1)
-    VALUES ('-', NOW(), 1.0, '-');
-""")
+        INSERT INTO counter (market_title, created_at, market_value, p1)
+        VALUES (%s, NOW(), %s, %s);
+    """, (unique_title, 1.0, "-"))
     conn.commit()
