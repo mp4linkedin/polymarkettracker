3e90b0a1cdab7b13edba72f829aeb471f9c4f904
mp4linkedin <136432267+mp4linkedin@users.noreply.github.com>
2025-06-16 16:41:33 +0200

    Update pmt.py

--- pmt.py
+++ pmt.py
@@ -65,24 +65,24 @@ for url in urls:
     
   
 
-        import streamlit as st
-        import psycopg2Add commentMore actions
-        from datetime import datetime
-        
-        conn = psycopg2.connect(
-            host="aws-0-eu-north-1.pooler.supabase.com",
-            database="postgres",
-            user="postgres.yoapzdfznvualrngslfs",
-            password="DDeras22",
-            port=6543
-        )
-        cursor = conn.cursor()
-        
-        params = st.query_params
-        if params.get("add", "").lower() == "true":
-            unique_title = "entry_" + datetime.utcnow().isoformat()
-            cursor.execute("""
-                INSERT INTO counter (market_title, created_at, market_value, p1)
-                VALUES (%s, NOW(), %s, %s);
-            """, (unique_title, 1.0, "-"))
-            conn.commit()
+    import streamlit as st
+    import psycopg2Add commentMore actions
+    from datetime import datetime
+    
+    conn = psycopg2.connect(
+        host="aws-0-eu-north-1.pooler.supabase.com",
+        database="postgres",
+        user="postgres.yoapzdfznvualrngslfs",
+        password="DDeras22",
+        port=6543
+    )
+    cursor = conn.cursor()
+    
+    params = st.query_params
+    if params.get("add", "").lower() == "true":
+        unique_title = "entry_" + datetime.utcnow().isoformat()
+        cursor.execute("""
+            INSERT INTO counter (market_title, created_at, market_value, p1)
+            VALUES (%s, NOW(), %s, %s);
+        """, (unique_title, 1.0, "-"))
+        conn.commit()
