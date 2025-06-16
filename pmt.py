1a7132076297eb7bca2c7939bace7438c9553c60
mp4linkedin <136432267+mp4linkedin@users.noreply.github.com>
2025-06-16 14:17:47 +0200

    Update pmt.py

--- pmt.py
+++ pmt.py
@@ -64,4 +64,23 @@ except requests.exceptions.RequestException as e:
 except (json.JSONDecodeError, KeyError) as e:
     st.error(f"Failed to parse the data: {e}")
 
+import streamlit as st
+import psycopg2
+from datetime import datetime
+
+conn = psycopg2.connect(
+    host="aws-0-eu-north-1.pooler.supabase.com",
+    database="postgres",
+    user="postgres.yoapzdfznvualrngslfs",
+    password="DDeras22",
+    port=6543
+)
+cursor = conn.cursor()
+
+params = st.query_params
+if params.get("add", "").lower() == "true":
+    unique_title = "entry_" + datetime.utcnow().isoformat()
+    cursor.execute("""INSERT INTO counter (market_title, created_at, market_value, p1) VALUES (%s, NOW(), %s, %s);""", (title, yes_price, "-"))
+    conn.commit()
+
 
